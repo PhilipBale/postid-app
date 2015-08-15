61890c2a7ae0f8e0dbf9bd427432c35ac7414584
Philip Bale <pbale95@gmail.com>
2015-08-15 16:16:31 -0700

    Initial commit

---
+++ Postid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Postid
+//
+//  Created by Philip Bale on 8/15/15.
+//  Copyright (c) 2015 Philip Bale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Postid/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Postid
+//
+//  Created by Philip Bale on 8/15/15.
+//  Copyright (c) 2015 Philip Bale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
