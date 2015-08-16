b69610be57a17e0d1e861f8591fbd4e0f36676a6
Philip Bale <pbale95@gmail.com>
2015-08-15 22:09:24 -0700

    iOS project setup

---
+++ Postid/AppEntryViewController.h
@@ -0,0 +1,16 @@
+//
+//  AppEntryViewController.h
+//  Postid
+//
+//  Created by Philip Bale on 8/15/15.
+//  Copyright (c) 2015 Philip Bale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppEntryViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *signupButton;
+
+@end
---
+++ Postid/AppEntryViewController.m
@@ -0,0 +1,39 @@
+//
+//  AppEntryViewController.m
+//  Postid
+//
+//  Created by Philip Bale on 8/15/15.
+//  Copyright (c) 2015 Philip Bale. All rights reserved.
+//
+
+#import "AppEntryViewController.h"
+
+@interface AppEntryViewController ()
+
+@end
+
+@implementation AppEntryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.loginButton.layer.cornerRadius = 10;
+    self.signupButton.layer.cornerRadius = 10;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
